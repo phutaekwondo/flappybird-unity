926d0c150630a12ae77ae9dde1421154f3e4579d
Vinh Phu <19522020@gm.uit.edu.vn>
2022-10-13 21:50:54 +0700

    add up key for jump, modify jump force and mass

--- Assets/JumpController.cs
+++ Assets/JumpController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class JumpController : MonoBehaviour
 {
-    //make the bird jump when the space key is pressed
+    //make the bird jump when the space key or the up key is pressed
 
     [SerializeField] private float jumpForce = 10f;
     public Rigidbody2D rb;
@@ -17,7 +17,7 @@ public class JumpController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             rb.velocity = Vector2.up * jumpForce;
         }
