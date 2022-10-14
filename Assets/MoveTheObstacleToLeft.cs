0195769e36de9ef9f06362c55a75e5ae8c873cc4
Vinh Phu <19522020@gm.uit.edu.vn>
2022-10-14 20:21:09 +0700

    add a pair of pipes, make it move to the left with speed of 5

---
+++ Assets/MoveTheObstacleToLeft.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTheObstacleToLeft : MonoBehaviour
+{
+    //move the obstacle to left with a speed of 5
+
+    public float speed = 5f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
